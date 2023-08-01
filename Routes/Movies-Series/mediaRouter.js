4f75f30b66453cabc8049edf68a92fca316335f3
Gonzalo Fernandez <ezekyel34@gmail.com>
2023-08-01 03:07:37 -0300

    cambios menores

--- Routes/Movies-Series/mediaRouter.js
+++ Routes/Movies-Series/mediaRouter.js
@@ -1,12 +1,13 @@
 const { Router } = require('express');
 // Acá habría que importar los handlers :)
+const { getMedia, mediaByID, postMedia } = require('../../controllers/media/mediaHandler');
 
 const mediaRouter = Router();
 
 
-mediaRouter.get('/', )  // Osea,  '/media';  Faltarían los handlers.
-mediaRouter.get('/:id', ) // Osea, '/media/id';  Faltarían los handlers.
-mediaRouter.post('/', ) // Faltaría el handler
+mediaRouter.get('/', getMedia)  // Osea,  '/media';  Faltarían los handlers.
+mediaRouter.get('/:id', mediaByID) // Osea, '/media/id';  Faltarían los handlers.
+mediaRouter.post('/', postMedia) // Faltaría el handler
 
 
 module.exports = mediaRouter;
--- controllers/media/mediaHandler.js
+++ controllers/media/mediaHandler.js
@@ -1,4 +1,41 @@
 // Acá irían los handlers para las movies y series (media)
-const { getAllMedia } = require('./mediaController');
+const { getAllMedia, getMediaById, postNewMedia } = require('./mediaController');
 
-const getMedia = async () => {}
+const getMedia = async (req, res) => {
+    try {
+        const response = await getAllMedia();
+        res.status(200).json(response);
+    } catch (error) {
+        res.status(400).json({ error: 'para la cola' })
+    }
+}
+
+
+const mediaByID = async (req, res) => {
+    let { id } = req.params
+    try {
+        const response = await getMediaById(id)
+        res.status(200).json(response);
+
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
+}
+
+
+const postMedia = async (req, res) => {
+    let {} = req.body  // los datos que necesitemos para postear
+    try {
+        const response = await postNewMedia() // la info que necistamos para postear.
+        res.status(200).json(response)
+
+    } catch (error) {
+        res.status(400).json({ error: 'para la cola' })
+    }
+}
+
+module.exports = {
+    getMedia,
+    mediaByID,
+    postMedia,
+}
