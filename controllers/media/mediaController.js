ed0a8248179437211caf40ed7f816f276cd1c72b
Gonzalo Fernandez <ezekyel34@gmail.com>
2023-08-02 09:51:59 -0300

    limpieza

--- controllers/media/mediaController.js
+++ controllers/media/mediaController.js
@@ -1,78 +1,12 @@
 const { Multimedia, Genres } = require('../../config/database');
-const { apiMovie, genres } = require('../../apiData/apiMovie');
 const { Op } = require('sequelize');
-
-  // Cargamos la db con los datos de la fake API:  (esto es para las pruebas)
-const loadMultimedia = async() => {
-  try {
-    // Verificamos si ya existen registros en la tabla Multimedia
-    const count = await Multimedia.count();
-    if (count > 0) {
-      console.log('La tabla Multimedia ya está poblada. No es necesario cargar los datos nuevamente.');
-      return;
-    }
-
-    // Mapeamos la api de movies y traemos la info que queramos:
-    const moviesMapped = apiMovie.map((ele) => ({
-      type: ele.type,
-      name: ele.name,
-      description: ele.description,
-      time: 120,
-      linkVideo: ele.linkVideo,
-      image: ele.image,
-      active: true,
-      price: 5.5,
-    }));
-
-    // Cargamos a Multimedia usando bulkCreate
-    const createdMedia = await Multimedia.bulkCreate(moviesMapped);
-
-    // Verificamos si todos los registros se crearon con éxito
-    if (createdMedia.length !== moviesMapped.length) {
-      throw new Error('Error loading multimedia: Some records were not created.');
-    }
-
-    console.log('Datos de multimedia cargados correctamente.');
-  } catch (error) {
-    console.error('Error loading multimedia:', error);
-    throw new Error('Error loading multimedia');
-  }
-
-}
-
-// Esto es de prueba, podemos modificarlo para que quede mejor
-const loadGenres = async() => {
-  
-    // Hacemos lo mismo con la api de genres:
-    const genresMapped = genres.map( ele => {
-      return {
-        name: ele.name,
-      }
-    } );
-
-    // Cargamos Genres:
-    const generos = await Genres.bulkCreate(genresMapped);
-
-    return generos;
-}
 
 
 const getAllMedia = async(name) => {
-    const countMedia = await Multimedia.count();  // con '.count()' obtenemos el número de registros 
-    const countGenre = await Genres.count();
-
-    if( countMedia === 0 ) { // Si no hay nada, entonces activamos la función para cargar Multimedia
-      await loadMultimedia();
-    }
-    if( countGenre === 0 ) { // Si no hay nada, entonces activamos la función para cargar Genres
-      await loadGenres();
-    }
 
     const media = await Multimedia.findAll({
       include: {
         model: Genres,
-        attributes: ['name'],
-        through: { attributes: [] },
       },
       where: name
         ? {
@@ -113,39 +47,26 @@ const getMediaById = async( id ) => {
 
 
 
-const isValidUrl = (url) => {
-  // Expresión regular para validar URLs generales
-  const urlPattern = /^https?:\/\/\S+$/;
-  return urlPattern.test(url);
-};
-
-const isValidImageUrl = (url) => {
-  // Expresión regular para validar URLs de imágenes
-  const imagePattern = /\.(jpeg|jpg|gif|png|svg)$/i;
-  return imagePattern.test(url);
-};
-
-
-
 const postNewMedia = async(type, name, description, time, linkVideo, image, price, genres) => {
 
-  // VALIDACIONES ( SAQUÉ TIME, PRICE HASTA QUE LO AGREGUEMOS A LA API :) )
-
-  if (!type || typeof type !== 'string' || !name || typeof name !== 'string' || 
-      !description || typeof description !== 'string' || 
-      typeof time !== 'number' || 
-      !linkVideo || typeof linkVideo !== 'string' || !isValidUrl(linkVideo) || 
-      !image || typeof image !== 'string' || !isValidImageUrl(image) || 
-      typeof price !== 'number' || isNaN(price) || !isFinite(price) || 
-      genres.length === 0) {
-    throw new Error('Invalid Data: Please provide valid values for all required fields.');
-  }
-
-
+  // Conversión del nombre de la peli a minúsculas:
+    const minName = name.toLowerCase();
+  
+  // Verificar si ya existe en la db para evitar repeticiones:
+    
     if( type === 'movie' ) {
+        const existingMovie = await Multimedia.findOne({
+          where: { name: { [Op.iLike]: minName } },
+        });
+
+        if( existingMovie ) {
+          throw new Error( 'The movie already exists' );
+        }
+  
+  // Si no existe, la creamos: 
         let movieCreate = await Multimedia.create({
             type,
-            name,
+            name: minName, // Creamos la peli con el name en minúsculas
             description,
             time,
             linkVideo,
@@ -154,7 +75,7 @@ const postNewMedia = async(type, name, description, time, linkVideo, image, pric
         });
 
     for (const gen of genres) {
-      const [genre] = await Genres.findOrCreate({
+      const [genre] = await Genres.findAll({
         where: { name: gen },
       });
 
