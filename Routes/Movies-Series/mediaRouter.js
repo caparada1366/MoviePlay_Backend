7d08422b7b75d8a1048eddaed72bcd5d27244fd9
NicolasSolans <nico.solans.drc@gmail.com>
2023-08-02 13:26:43 -0300

    nada

--- Routes/Movies-Series/mediaRouter.js
+++ Routes/Movies-Series/mediaRouter.js
@@ -1,13 +1,10 @@
-const { Router } = require('express');
+const { Router } = require("express");
 // Acá habría que importar los handlers :)
 
 const mediaRouter = Router();
 
-
-mediaRouter.get('/', )  // Osea,  '/media';  Faltarían los handlers.
-mediaRouter.get('/:id', ) // Osea, '/media/id';  Faltarían los handlers.
-mediaRouter.post('/', ) // Faltaría el handler
-
+mediaRouter.get("/"); // Osea,  '/media';  Faltarían los handlers.
+mediaRouter.get("/:id"); // Osea, '/media/id';  Faltarían los handlers.
+mediaRouter.post("/"); // Faltaría el handler
 
 module.exports = mediaRouter;
-
