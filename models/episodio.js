1367d1469b777b0e173e8f5e9f70e331c2c44f85
NicolasSolans <nico.solans.drc@gmail.com>
2023-08-02 17:41:00 -0300

    cambios de export en los modelos

--- models/episodio.js
+++ models/episodio.js
@@ -1,39 +1,43 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('./database');
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
 
-const Episodios = sequelize.define('Episodios', {
-  episodioId: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  numEpisodio: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-  numTemporada:{
-    type: DataTypes.INTEGER,
-    allowNull: false
-  },
-  titulo: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  descripcion: {
-    type: DataTypes.TEXT,
-    allowNull: false
-  },
-  linkVideo: {
-    type: DataTypes.STRING,
-    allowNull: false
-},
-  duracion: {
-    type: DataTypes.STRING,
-    allowNull: false
-  }
-},
-{ 
-  timestamps: false,
-});
-
-module.exports = Episodios;
+module.exports = (sequelize) => {
+  const Episodios = sequelize.define(
+    "Episodios",
+    {
+      episodioId: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      numEpisodio: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      numTemporada: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      titulo: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      descripcion: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      linkVideo: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      duracion: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      timestamps: false,
+    }
+  );
+  return Episodios;
+};
--- models/series.js
+++ models/series.js
@@ -1,29 +1,33 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('./database');
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
 
-const Series = sequelize.define('Series', {
-  serieId: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  titulo: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  descripcion: {
-    type: DataTypes.TEXT,
-  },
-  yearEstreno: {
-    type: DataTypes.INTEGER,
-  },
-  actores: {
-    type: DataTypes.ARRAY(DataTypes.STRING),
-    allowNull: false,
-  },
-}, 
-{ 
-  timestamps: false,
-});
-
-module.exports = Series;
+module.exports = (sequelize) => {
+  const Series = sequelize.define(
+    "Series",
+    {
+      serieId: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      titulo: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      descripcion: {
+        type: DataTypes.TEXT,
+      },
+      yearEstreno: {
+        type: DataTypes.INTEGER,
+      },
+      actores: {
+        type: DataTypes.ARRAY(DataTypes.STRING),
+        allowNull: false,
+      },
+    },
+    {
+      timestamps: false,
+    }
+  );
+  return Series;
+};
