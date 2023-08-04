1a834ad9fe07978c45deed0718db5261478d384e
Gonzalo Fernandez <ezekyel34@gmail.com>
2023-08-04 00:14:02 -0300

    nada

--- Routes/Movies-Series/mediaRouter.js
+++ Routes/Movies-Series/mediaRouter.js
@@ -9,6 +9,7 @@ const getMoviesYSeries = require("../../controllers/getMoviesYSeries");
 const getSeries = require("../../controllers/getSeries")
 const getMovies = require("../../controllers/getMovies");
 const serieByID = require("../../controllers/getSerieByID");
+const { postSeries } = require("../../controllers/postSeries");
 
 const mediaRouter = Router();
 
@@ -19,5 +20,6 @@ mediaRouter.get("/", getMedia);
 mediaRouter.get("/:id", mediaByID);
 mediaRouter.get("/series/:id", serieByID);
 mediaRouter.post("/", postMedia);
+mediaRouter.post("/series", postSeries);
 
 module.exports = mediaRouter;
