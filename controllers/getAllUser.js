9fd27b385e800f5aca77b996b30d0a2d4a452830
NicolasSolans <nico.solans.drc@gmail.com>
2023-08-10 11:01:09 -0300

    controller getAllUser

--- controllers/getAllUser.js
+++ controllers/getAllUser.js
@@ -1,8 +1,12 @@
 const {Usuario} = require('../config/database')
 
 const getAllUser = async(req, res) => {
-    const response = await Usuario.findAll()
-    res.status(200).json(response)
+    try {
+        const response = await Usuario.findAll()
+        res.status(200).json(response)
+    } catch (error) {
+        res.status(404).json({error: error.message})
+    }
 };
 
 module.exports = {getAllUser}
