f92312059374d13a641adbe8bb669f5d02ee5322
Camilo Parada Ruiz <ca.parada1366@gmail.com>
2023-08-15 17:18:10 -0500

    getmovies y series admin

--- controllers/ordenesCompra/getAllOCs.js
+++ controllers/ordenesCompra/getAllOCs.js
@@ -2,13 +2,34 @@ const { Series, Usuario, Multimedia, OrdenDeCompra } = require('../../config/dat
 
 const getAllOCs = async (req, res)=>{
     try {
+        const {page} = req.query;   
+        const pageNumber = parseInt(page) || 1;
+        const pageSize = 10;
+        const offset = (pageNumber - 1) * pageSize;
         
-            const arrayOC = await OrdenDeCompra.findAll({
+            const arrayOC= await OrdenDeCompra.findAll({
                 include: [
                     {model: Series, attributes: ['serieId', 'titulo', 'image', 'price']},
-                    {model: Multimedia, attributes: ['id', 'name', 'image', 'price']}]
+                    {model: Multimedia, attributes: ['id', 'name', 'image', 'price']}],
+                distinct: true,
+                offset: offset,
+                limit: pageSize, 
             })
-            res.status(200).send(arrayOC)
+           const totalPages = Math.ceil(arrayOC.length / pageSize);
+           
+            var ventaTotal = 0;
+            arrayOC.forEach(element => {
+                ventaTotal += (element.total)/100
+            });
+
+            const respuesta = {
+                totalOCs: arrayOC.lenght,
+                ventaTotal,
+                totalPages,
+                currentPage: pageNumber,
+                OCs: arrayOC
+            }
+            res.status(200).send(respuesta)
         
 
        
--- controllers/ordenesCompra/getOCxUser.js
+++ controllers/ordenesCompra/getOCxUser.js
@@ -1,16 +1,36 @@
 const { Series, Usuario, Multimedia, OrdenDeCompra } = require('../../config/database');
 
 const getOcXUser = async (req, res)=>{
-    const {idUser} = req.body;
+    const {idUser, page} = req.body;
+    const pageNumber = parseInt(page) || 1;
+    const pageSize = 10;
+    const offset = (pageNumber - 1) * pageSize;
     try {
         if(idUser){
             const arrayOC = await OrdenDeCompra.findAll({
                 where: {usuarioId: idUser},
                 include: [
                     {model: Series, attributes: ['serieId', 'titulo', 'image', 'price']},
-                    {model: Multimedia, attributes: ['id', 'name', 'image', 'price']}]
+                    {model: Multimedia, attributes: ['id', 'name', 'image', 'price']}],
+                distinct: true,
+                offset: offset,
+                limit: pageSize, 
             })
-            res.status(200).send(arrayOC)
+            const totalPages = Math.ceil(arrayOC.length / pageSize);
+           
+            var ventaTotal = 0;
+            arrayOC.forEach(element => {
+                ventaTotal += (element.total)/100
+            });
+
+            const respuesta = {
+                totalOCs: arrayOC.lenght,
+                ventaTotal,
+                totalPages,
+                currentPage: pageNumber,
+                OCs: arrayOC
+            }
+            res.status(200).send(respuesta)
         }
 
        
