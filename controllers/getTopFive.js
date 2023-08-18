73fef5707b06ab21a457688f7ddd75b537ed10b5
NicolasSolans <nico.solans.drc@gmail.com>
2023-08-17 22:12:21 -0300

    nada

--- controllers/getTopFive.js
+++ controllers/getTopFive.js
@@ -8,24 +8,12 @@ const topFiveMovie = async (req, res) => {
 
         const videoIds = movies.map((m) => {
             const videoUrl = new URL(m.linkVideo)
-            //  return videoUrl.split('v=')[1];
+            
             return videoUrl.searchParams.get('v')
         });
 
-        // const response = await axios.get('https://www.googleapis.com/youtube/v3/videos', {
-        //     params: {
-        //         key: apiKey,
-        //         id: videoIds.join(','),
-        //         part: 'statistics'
-        //     }
-        // });
-        // const data = response.data.items
 
-        // const viewCounts = response.data.items.map(item => item.statistics.viewCount);
-        
-
-
-        res.status(200).json('hola');
+        res.status(200).json(videoIds);
     } catch (error) {
         res.status(404).json({error: error.message});
     }
