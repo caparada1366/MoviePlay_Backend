bdf8836759499a5a5e79cc9cae211fbb27d438bf
Camilo <camilojpg30@gmail.com>
2023-08-21 22:28:40 -0300

    para hacer pull

--- controllers/postUser.js
+++ controllers/postUser.js
@@ -83,14 +83,15 @@ const postUser = async(req, res) => {
             await nuevoUsuario.save();
 
             res.cookie('token', token)
+
             enviarCorreoBienvenida(email, nombre, apellido);
+            console.log(res);
             return res.status(201).json({
                 message: 'Usuario creado con éxito',
-                id: userSaved.id,
-                nombre: userSaved.nombre,
-                apellido: userSaved.apellido,
-                email: userSaved.email     
+                userSaved,
+                token   
             });
+            
         }
     } catch (error) {
         res.status(404).json({error: error.message})
